40c4eaffd59ce91eee6a82416336fbb5a4e8b954
Justin Hallier <justin.hallier+git@gmail.com>
2024-07-08 20:27:11 +1200

    add sqlite module

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"os"
 
 	"github.com/charmbracelet/log"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+// What needs to happen:
+// A user registers their interest with their name and is given the ntfy topic link
+// the app subscribes to laundry events on mqtt
+// Home assistant will publish messages to this topic
+// If a user has registered interest, send the next 'finished' message to the ntfy topic + the users name
+// Delete the user's "interest"
 
 func main() {
 	log.SetLevel(log.DebugLevel)
 	log.Info("Starting application")
 	log.Info("Log set to " + log.GetLevel().String())
+
+	db, err := sql.Open("sqlite3", "data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table if not exists users (id integer not null primary key, name text, created_at date);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
 	opts := MQTT.NewClientOptions()
 	opts.AddBroker("mqtt://10.0.0.3:1883")
 	opts.SetClientID("laundry-notify")
@@ -42,6 +67,4 @@ func main() {
 		log.Debug("RECEIVED TOPIC: %s MESSAGE: %s\n", "topic", incoming[0], "message", incoming[1])
 	}
 
-	// client.Disconnect(250)
-	// log.Info("Sample Subscriber Disconnected")
 }
