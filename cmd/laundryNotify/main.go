d1bb8a283358289ebbe6b288b6564dcf19f4c331
Justin Hallier <justin.hallier+git@gmail.com>
2024-07-22 21:59:35 +1200

    Update the main file
    Should now be fairly accurate to the example repo.
    Once I build out the services some more, I'll finish
    up the rest of what's needed in m.Run()

--- cmd/laundryNotify/main.go
+++ cmd/laundryNotify/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"jallier/laundry-notify/internal/sqlite"
-	"log"
 	"os"
 	"os/signal"
 
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
 	"golang.org/x/net/context"
 )
 
 func main() {
+	log.Info("starting...")
+
 	// Setup signal handlers
 	ctx, cancel := context.WithCancel(context.Background())
 	c := make(chan os.Signal, 1)
@@ -18,6 +21,18 @@ func main() {
 
 	m := NewMain()
 
+	// parse env vars and load config
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file. Either one not provided or running in prod mode")
+	}
+	env := os.Getenv("ENV")
+	if env == "dev" || env == "development" {
+		log.SetLevel(log.DebugLevel)
+	}
+	SetConfigFromEnv(&m.Config)
+
+	// Execute the application
 	if err := m.Run(ctx); err != nil {
 		log.Printf("error: %v", err)
 		m.Close()
@@ -26,6 +41,7 @@ func main() {
 
 	// Wait for ctrl c
 	<-ctx.Done()
+	log.Info("shutting down...")
 
 	if err := m.Close(); err != nil {
 		log.Printf("error: %v", err)
@@ -34,15 +50,19 @@ func main() {
 }
 
 type Main struct {
-	DB *sqlite.DB
+	DB     *sqlite.DB
+	Config Config
 }
 
+// Returns a new instance of Main
 func NewMain() *Main {
 	return &Main{
-		DB: sqlite.NewDB(""),
+		DB:     sqlite.NewDB(""),
+		Config: *DefaultConfig(),
 	}
 }
 
+// Close gracefully shuts down the application
 func (m *Main) Close() error {
 	if m.DB != nil {
 		if err := m.DB.Close(); err != nil {
@@ -53,6 +73,43 @@ func (m *Main) Close() error {
 	return nil
 }
 
+// Run starts the application. The config must be loaded before calling this method
 func (m *Main) Run(ctx context.Context) (err error) {
+	// This is where the application logic would go
+	// For now, we just print the config
+	log.Debug("config: ", "config", m.Config)
 	return nil
+}
+
+const DefaultDSN = "data.db"
+
+// Config represents the application configuration
+type Config struct {
+	MQTT struct {
+		URL      string
+		ClientId string
+		Username string
+		Password string
+		topic    string
+	}
+	DB struct {
+		DSN string
+	}
+}
+
+// DefaultConfig returns a new instance of Config with default values
+func DefaultConfig() *Config {
+	var config Config
+	config.DB.DSN = DefaultDSN
+
+	return &config
+}
+
+func SetConfigFromEnv(config *Config) {
+	config.DB.DSN = os.Getenv("DB_DSN")
+	config.MQTT.URL = os.Getenv("MQTT_URL")
+	config.MQTT.ClientId = os.Getenv("MQTT_CLIENT_ID")
+	config.MQTT.Username = os.Getenv("MQTT_USERNAME")
+	config.MQTT.Password = os.Getenv("MQTT_PASSWORD")
+	config.MQTT.topic = os.Getenv("MQTT_TOPIC")
 }
