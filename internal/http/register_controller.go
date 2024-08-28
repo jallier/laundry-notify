e913726d83328a541292a8aed2290570bd491828
Justin Hallier <justin.hallier+git@gmail.com>
2024-08-28 21:10:00 +1200

    start updating the register screen

--- internal/http/register_controller.go
+++ internal/http/register_controller.go
@@ -24,14 +24,14 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 	c.Bind(&req)
 
 	if req.Name == "" {
-		c.HTML(http.StatusOK, "register", gin.H{
+		c.HTML(http.StatusOK, "registered", gin.H{
 			"error": "Name is required",
 		})
 		return
 	}
 
 	if req.Type == "" || (req.Type != laundryNotify.WASHER_EVENT && req.Type != laundryNotify.DRYER_EVENT) {
-		c.HTML(http.StatusOK, "register", gin.H{
+		c.HTML(http.StatusOK, "registered", gin.H{
 			"error": "Valid type is required",
 		})
 		return
@@ -47,7 +47,7 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 		err = s.UserService.CreateUser(s.ctx, user)
 		if err != nil {
 			log.Error("Error creating user", "error", err)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "Error creating user",
 			})
 			return
@@ -60,21 +60,21 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 	mostRecentEvent, err := s.EventService.FindMostRecentEvent(s.ctx, req.Type)
 	if err != nil {
 		log.Error("Error finding most recent event", "error", err)
-		c.HTML(http.StatusOK, "register", gin.H{
+		c.HTML(http.StatusOK, "registered", gin.H{
 			"error": "Error finding most recent event",
 		})
 		return
 	}
 
 	// If finished at isn't set, then this event is ongoing
-	if mostRecentEvent.FinishedAt.Valid {
+	if mostRecentEvent != nil && mostRecentEvent.FinishedAt.Valid {
 		log.Debug("Event is finished", "event", mostRecentEvent)
 		// Event is finished
 		// First check if they have already registered for the next event
 		_, n, err := s.UserEventService.FindByUserName(s.ctx, user.Name, req.Type)
 		if err != nil {
 			log.Error("Error finding user events by name", "error", err)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "Error finding user events by name",
 			})
 			return
@@ -82,7 +82,7 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 		log.Debug("User event count", "count", n)
 		if n > 0 {
 			log.Info("User already registered for next event", "user", user)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "User already registered for next event",
 			})
 			return
@@ -95,7 +95,7 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 		err = s.UserEventService.CreateUserEvent(s.ctx, userEvent)
 		if err != nil {
 			log.Error("Error creating user event", "error", err)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "Error creating user event",
 			})
 			return
@@ -108,14 +108,14 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 		_, n, err := s.UserEventService.FindByUserName(s.ctx, user.Name, req.Type)
 		if err != nil {
 			log.Error("Error finding user events by name", "error", err)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "Error finding user events by name",
 			})
 			return
 		}
 		if n > 0 {
 			log.Info("User already registered for this event", "user", user)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "User already registered for this event",
 			})
 			return
@@ -129,13 +129,13 @@ func (s *HttpServer) handleRegister(c *gin.Context) {
 		err = s.UserEventService.CreateUserEvent(s.ctx, userEvent)
 		if err != nil {
 			log.Error("Error creating user event", "error", err)
-			c.HTML(http.StatusOK, "register", gin.H{
+			c.HTML(http.StatusOK, "registered", gin.H{
 				"error": "Error creating user event",
 			})
 			return
 		}
 		log.Info("User registered for ongoing event", "user", user, "event", mostRecentEvent)
-		c.HTML(http.StatusOK, "register", gin.H{
+		c.HTML(http.StatusOK, "registered", gin.H{
 			"success": "User registered for ongoing event",
 		})
 	}
