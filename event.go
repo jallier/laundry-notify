8567a522ce19f5bf0535e9d54b9e0eaebcb6c435
Justin Hallier <justin.hallier+git@gmail.com>
2024-08-31 23:18:16 +1200

    remove old code

--- event.go
+++ event.go
@@ -1,7 +1,9 @@
 package laundryNotify
 
 import (
+	"context"
 	"database/sql"
+	"time"
 )
 
 const WASHER_EVENT = "washer"
@@ -34,4 +36,34 @@ func (e *Event) Validate() error {
 // Represents a set of fields to update on an event
 type EventUpdate struct {
 	FinishedAt sql.NullTime
+}
+
+type EventFilter struct {
+	Id         *int
+	Type       *string
+	StartedAt  time.Time
+	FinishedAt time.Time
+	Limit      int
+	Offset     int
+	OrderBy    []string
+}
+
+type EventService interface {
+	FindEventById(ctx context.Context, userId int) (*Event, error)
+	FindMostRecentEvent(ctx context.Context, eventType string) (*Event, error)
+	CreateEvent(ctx context.Context, event *Event) error
+	UpdateEvent(ctx context.Context, id int, update EventUpdate) (*Event, error)
+}
+
+type UserEventFilter struct {
+	Id *int
+}
+
+type UserEventService interface {
+	FindUserEventById(ctx context.Context, id int) (*UserEvent, error)
+	FindUserNamesByEventId(ctx context.Context, eventId int) ([]string, error)
+	FindByUserName(ctx context.Context, name string, eventType string) ([]*UserEvent, int, error)
+	FindUpcomingUserEvents(ctx context.Context, eventType string) ([]*UserEvent, int, error)
+	CreateUserEvent(ctx context.Context, userEvent *UserEvent) error
+	UpdateUserEvent(ctx context.Context, id int, update UserEventUpdate) (*UserEvent, error)
 }
